90389c404d2efc9babc22d4a908eb9932c5b0c78
Iker Arias <iker.arias.martinez@opendeusto.es>
2024-12-07 21:25:37 +0100

    Idioma a Italino e Ingles

--- empresaLibros/urls.py
+++ empresaLibros/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.views.i18n import set_language  # Añadir esto
+from django.views.i18n import set_language  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
